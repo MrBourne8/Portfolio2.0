895b55b34fe428515c4552dc5bd3fd2086e53fd5
MrBourne8 <antohas8@rambler.ru>
2019-01-31 19:26:58 +0700

    Обновочка

--- 02/js/common.js
+++ 02/js/common.js
@@ -1,30 +1,29 @@
 var link = $('.menu-link');
-      var menu = $('.menu');
-      var nav_link = $('.menu a');
+var menu = $('.menu');
+var nav_link = $('.menu a');
 
-      link.click(function(){
-        link.toggleClass('menu-link_active');
-        menu.toggleClass('menu_active');
-      });
-      nav_link.click(function(){
-        link.toggleClass('menu-link_active');
-        menu.toggleClass('menu_active');
-      })
+link.click(function () {
+  link.toggleClass('menu-link_active');
+  menu.toggleClass('menu_active');
+});
+nav_link.click(function () {
+  link.toggleClass('menu-link_active');
+  menu.toggleClass('menu_active');
+});
 
-$("a[href*=#]").on("click", function(e){
+$("a[href*=#]").on("click", function (e) {
   var anchor = $(this);
   $('html, body').stop().animate({
-      scrollTop: $(anchor.attr('href')).offset().top
+    scrollTop: $(anchor.attr('href')).offset().top
   }, 777);
   e.preventDefault();
   return false;
 });
 
-$(window).scroll(function() {
-if ($(this).scrollTop() > 1){
-$('header').addClass("clone");
-}
-else{
-$('header').removeClass("clone");
-}
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 1) {
+    $('header').addClass("clone");
+  } else {
+    $('header').removeClass("clone");
+  }
 });
