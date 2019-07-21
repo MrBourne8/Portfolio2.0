13a0fdf05b9c7b7cd2e881290d044f3eb7697210
MrBourne8 <antohas8@rambler.ru>
2019-07-21 21:26:01 +0700

    Доработка

--- 333/index.php
+++ 333/index.php
@@ -381,25 +381,7 @@
 	</section>
 	<!-- Название секции End-->
 	<!-- Подвал -->
-	<footer>
-		<div class="container">
-			<div class="header__wrap">
-				<div class="logo">
-					<img src="img/logo.svg" alt="Logo">
-				</div>
-				<div class="header__geo">
-					<img src="img/icons/geo.png" alt="Geo">
-					<p>
-						Работаем по Всей Московской области
-					</p>
-				</div>
-				<div class="header__phone">
-					<p>8 965 131 64 60</p>
-					<button class="btn btn-mod">Оставить заявку</button>
-				</div>
-			</div>
-		</div>
-	</footer>
+	<?php include ("blocks/footer.php");?>
 	<!-- Подвал End-->
 
 	<div class="modal">
