0e2a0cbe3f1d9074e754c0330b5c5ea2362883d4
MrBourne8 <antohas8@rambler.ru>
2019-07-23 02:42:08 +0700

    Работа

---
+++ 888/work/common.js
@@ -0,0 +1,21 @@
+	$(".yap").hover(function () {
+		$('.strel').toggleClass("yap_deg");
+	});
+	$(".ger").hover(function () {
+		$('.strel').toggleClass("ger_deg");
+	});
+	$(".por").hover(function () {
+		$('.strel').toggleClass("por_deg");
+	});
+	$(".chi").hover(function () {
+		$('.strel').toggleClass("chi_deg");
+	});
+	$(".iz").hover(function () {
+		$('.strel').toggleClass("iz_deg");
+	});
+	$(".ben").hover(function () {
+		$('.strel').toggleClass("ben_deg");
+	});
+	$(".tay").hover(function () {
+		$('.strel').toggleClass("tay_deg");
+	});
