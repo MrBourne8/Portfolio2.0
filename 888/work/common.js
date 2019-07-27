0118be0dfb7d1fe7253d4106c035625fa91701d4
MrBourne8 <antohas8@rambler.ru>
2019-07-27 09:06:01 +0700

    8

--- 888/work/common.js
+++ 888/work/common.js
@@ -1,5 +1,6 @@
 	$(".yap").hover(function () {
 		$('.strel').toggleClass("yap_deg");
+		$('.strel').removeClass("iz_deg-l");
 	});
 	$(".ger").hover(function () {
 		$('.strel').toggleClass("ger_deg");
@@ -9,13 +10,21 @@
 	});
 	$(".chi").hover(function () {
 		$('.strel').toggleClass("chi_deg");
+		$('.strel').removeClass("iz_deg-l");
 	});
 	$(".iz").hover(function () {
 		$('.strel').toggleClass("iz_deg");
+		$('.strel').addClass("iz_deg-l");
+	});
+	$(".ben").hover(function () {
+		if ($('.strel').hasClass('iz_deg-l')) {
+			$('.strel').toggleClass("ben_deg-l");
+		}
 	});
 	$(".ben").hover(function () {
 		$('.strel').toggleClass("ben_deg");
 	});
 	$(".tay").hover(function () {
 		$('.strel').toggleClass("tay_deg");
+		$('.strel').removeClass("iz_deg-l");
 	});
