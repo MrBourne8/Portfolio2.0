0caf96c6b22cb16aa3ff71848486890222069805
MrBourne8 <antohas8@rambler.ru>
2019-12-28 23:50:07 +0700

    Поправил меню

--- 08/work/common.js
+++ 08/work/common.js
@@ -41,15 +41,18 @@ $(document).ready(function() {
     var link = $('.menu-link');
     var menu = $('.menu');
     var nav_link = $('.menu a');
+    var body = $('body');
 
     link.click(function() {
         link.toggleClass('menu-link_active');
         menu.toggleClass('menu_active');
+        body.toggleClass('no-scoll');
         e.preventDefault();
     });
     nav_link.click(function() {
         link.toggleClass('menu-link_active');
         menu.toggleClass('menu_active');
+        body.toggleClass('no-scoll');
         e.preventDefault();
 
     });
